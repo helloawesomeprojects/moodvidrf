2b49eecd9795d56e9c3726f57972470d4aea6986
Javokhir Jurakhonov <49615839+helloawesomeprojects@users.noreply.github.com>
2023-03-13 21:05:35 +0500

    Update settings.py

--- moodvi/settings.py
+++ moodvi/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'videos.apps.VideosConfig',
+    'corsheaders',
     'rest_framework',
     'djoser',
     'rest_framework.authtoken',
